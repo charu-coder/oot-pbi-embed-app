77a8a648f4df24cf58b7bef8a51071f0202058f7
charu <cagrawal@ootconsulting.com>
2025-07-31 23:15:53 +0530

    fixed syntax for token

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -77,8 +77,8 @@ const Home = () => {
       // return data; // Return token object
       setPowerBIToken(data.accessToken);
       console.log("before sending token bro", res)
-      await fetchEmbedUrl({ reportId: "5dd47363-29f8-4984-90d4-2c7e7fc947c5", workspaceId: "d98dbbf9-63bf-4c37-ac97-584e3997a220", accessToken: data.access_token })
-      const token = await generateEmbedToken({ reportId: "5dd47363-29f8-4984-90d4-2c7e7fc947c5", workspaceId: "d98dbbf9-63bf-4c37-ac97-584e3997a220", datasetId: "92d1baa2-a11a-4d28-baa6-c44b3d50a68d", accessToken: data.access_token });
+      await fetchEmbedUrl({ reportId: "5dd47363-29f8-4984-90d4-2c7e7fc947c5", workspaceId: "d98dbbf9-63bf-4c37-ac97-584e3997a220", accessToken: data.accessToken })
+      const token = await generateEmbedToken({ reportId: "5dd47363-29f8-4984-90d4-2c7e7fc947c5", workspaceId: "d98dbbf9-63bf-4c37-ac97-584e3997a220", datasetId: "92d1baa2-a11a-4d28-baa6-c44b3d50a68d", accessToken: data.accessToken });
 
       setEmbedToken(token);
     };
